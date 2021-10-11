71f900956090b1b8826b7f3981806af72c672662
Miranda <stillfree.ml@gmail.com>
2021-10-11 14:59:33 +0900

    revamp menu header

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,10 +6,12 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 500) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $("#home").addClass("active");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $("#home").removeClass("active");
     }
 });
 
